dc22b928a651c4036ad48ce8b2e4d11e70c5d666
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2024-04-15 10:47:44 -0400

    Delete app directory

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Document(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    file_path = db.Column(db.String(255), nullable=False)
