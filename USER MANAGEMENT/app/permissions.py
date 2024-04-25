e2d57359931caf8c6df91aa37976f6f66a29d226
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2024-04-25 17:19:29 -0400

    Delete USER MANAGEMENT directory

--- USER MANAGEMENT/app/permissions.py
+++
@@ -1,9 +0,0 @@
-from flask_principal import Permission, RoleNeed
-
-# Define role needs
-admin_role = RoleNeed('admin')
-guest_role = RoleNeed('guest')
-
-# Define permissions
-admin_permission = Permission(admin_role)
-guest_permission = Permission(guest_role)
